be6bfb36bb43ac6c1b43f126dfef866c984d80c3
mgd <mr.zizikk@gamil.com>
2023-06-05 16:30:19 -0400

    collect static files

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret-key')
 
-DEBUG = bool(int(os.getenv('DEBUG', False)))
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
