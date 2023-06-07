952ea1977aab57d58abac35d7994358e1f4db1f1
mgd <mr.zizikk@gamil.com>
2023-06-07 16:50:19 -0400

    user retrieve, update, partial update views ,serializers implementation

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth import login
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import User
 from .serializers import (
     UserCreateSerializer,
-    UserLoginSerializer
+    UserLoginSerializer,
+    UserRetrieveSerializer,
+    UserUpdateSerializer
 )
 
 
@@ -23,3 +28,27 @@ class UserLoginView(generics.GenericAPIView):
         user = serializer.save()
         login(request=request, user=user)
         return Response(serializer.data)
+
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = User
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = UserRetrieveSerializer
+        self.kwargs = {'pk': self.request.user.pk}
+
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        self.serializer_class = UserUpdateSerializer
+        self.kwargs = {'pk': self.request.user.pk}
+
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        self.serializer_class = UserUpdateSerializer
+        self.kwargs = {'pk': self.request.user.pk}
+
+        return super().partial_update(request, *args, **kwargs)
