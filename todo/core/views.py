f59bd5509b04d39cf5ea0d902472de61cc9862c0
mgd <mr.zizikk@gamil.com>
2023-06-07 00:42:29 -0400

    user login implementation

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,9 +1,25 @@
-from rest_framework.generics import CreateAPIView
+from django.contrib.auth import login
+from rest_framework import generics
+from rest_framework.response import Response
 
 from .models import User
-from .serializers import UserCreateSerializer
+from .serializers import (
+    UserCreateSerializer,
+    UserLoginSerializer
+)
 
 
-class UserCreateView(CreateAPIView):
+class UserCreateView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
     queryset = User
+
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
