dc7f10208f4c2023bd5fd45d79a8fdfcb6b9414b
mgd <mr.zizikk@gamil.com>
2023-06-08 20:16:31 -0400

    install social-auth-app-django, configure oauth settings and urls

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -9,7 +9,8 @@ from django.urls import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls'))
+    path('core/', include('core.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
