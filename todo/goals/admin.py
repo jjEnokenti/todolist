86da7dd89c183bec85cf87fd20f6c49e3517dd5a
mgd <mr.zizikk@gamil.com>
2023-06-10 15:21:02 -0400

    refactoring admin mapping for goal category

--- todo/goals/admin.py
+++ todo/goals/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import GoalCategory
 
 
-admin.site.register(GoalCategory)
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created', 'updated')
+    search_fields = ('title', 'created')
+
+
+admin.site.register(GoalCategory, GoalCategoryAdmin)
