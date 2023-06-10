69bfbe74bf2f5ba88d54fd3d537e35e79a5c5a6a
mgd <mr.zizikk@gamil.com>
2023-06-10 16:07:41 -0400

    create goal model and run migrate

--- todo/goals/models.py
+++ todo/goals/models.py
@@ -6,6 +6,20 @@ from django.utils import timezone
 user = get_user_model()
 
 
+class Status(models.IntegerChoices):
+    to_do = (1, 'К выполнению')
+    in_progress = (2, 'В процессе')
+    done = (3, 'Готово')
+    archived = (4, 'В архиве')
+
+
+class Priority(models.IntegerChoices):
+    low = (1, 'Низкий')
+    medium = (2, 'Средний')
+    high = (3, 'Высокий')
+    critical = (4, 'Критический')
+
+
 class GoalCategory(models.Model):
     class Meta:
         verbose_name = 'Категория'
@@ -22,6 +36,37 @@ class GoalCategory(models.Model):
             self.created = timezone.now()
         self.updated = timezone.now()
 
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Goal(models.Model):
+    class Meta:
+        verbose_name = 'Цель'
+        verbose_name_plural = 'Цели'
+
+    title = models.CharField(verbose_name='Название', max_length=155)
+    description = models.CharField(verbose_name='Описание', max_length=500)
+    category = models.ForeignKey(GoalCategory, verbose_name='Категория', on_delete=models.CASCADE)
+    status = models.PositiveSmallIntegerField(
+        verbose_name='Статус',
+        choices=Status.choices,
+        default=Status.to_do)
+    priority = models.PositiveSmallIntegerField(
+        verbose_name='Приоритет',
+        choices=Priority.choices,
+        default=Priority.medium
+    )
+    deadline = models.DateTimeField(verbose_name='Дата дедлайна')
+    created = models.DateTimeField(verbose_name='Дата создания')
+    updated = models.DateTimeField(verbose_name='Дата последнего обновления')
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.updated = timezone.now()
         return super().save(*args, **kwargs)
 
     def __str__(self):
