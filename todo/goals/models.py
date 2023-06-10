e187fddcaddcb44477e847030d46d492480dcba4
mgd <mr.zizikk@gamil.com>
2023-06-09 20:32:51 -0400

    create goal category model and make migrate

---
+++ todo/goals/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
+
+
+user = get_user_model()
+
+
+class GoalCategory(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    title = models.CharField(verbose_name='Название', max_length=255)
+    user = models.ForeignKey(user, verbose_name='Автор', on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(verbose_name='Удалена', default=False)
+    created = models.DateTimeField(verbose_name='Дата создания')
+    updated = models.DateTimeField(verbose_name='Дата последнего обновления')
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.updated = timezone.now()
+
+        return super().save(*args, **kwargs)
