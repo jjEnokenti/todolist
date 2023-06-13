1b7388888d57b4234117f5271681b369640a0215
mgd <mr.zizikk@gamil.com>
2023-06-12 21:16:50 -0400

    add to admin panel models goal, comment

--- todo/goals/admin.py
+++ todo/goals/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import GoalCategory
+from .models import (
+    Comment,
+    Goal,
+    GoalCategory
+)
 
 
 class GoalCategoryAdmin(admin.ModelAdmin):
@@ -8,4 +12,24 @@ class GoalCategoryAdmin(admin.ModelAdmin):
     search_fields = ('title', 'created')
 
 
+class GoalAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category',
+        'priority',
+        'status',
+        'due_date',
+        'created',
+        'updated'
+    )
+    search_fields = ('title', 'created')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'created', 'updated')
+    search_fields = ('text', 'created')
+
+
 admin.site.register(GoalCategory, GoalCategoryAdmin)
+admin.site.register(Goal, GoalAdmin)
+admin.site.register(Comment, CommentAdmin)
