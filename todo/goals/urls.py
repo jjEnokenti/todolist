d44d84a7cf58d60d764e7ee04ce4a6f55c87ef93
mgd <mr.zizikk@gamil.com>
2023-06-14 11:35:52 -0400

    implementation of work with boards

--- todo/goals/urls.py
+++ todo/goals/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import (
+    board,
     category,
     comment,
     goal
@@ -22,4 +23,9 @@ urlpatterns = [
     path('goal_comment/list', comment.CommentListView.as_view()),
     path('goal_comment/create', comment.CommentCreateView.as_view()),
     path('goal_comment/<int:pk>', comment.CommentView.as_view()),
+
+    # boards
+    path('board/list', board.BoardListView.as_view()),
+    path('board/create', board.BoardCreateView.as_view()),
+    path('board/<int:pk>', board.BoardView.as_view()),
 ]
