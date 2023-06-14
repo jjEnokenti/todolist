985a8534511e6bf3fd9f968c8cfbfa46f07d6857
mgd <mr.zizikk@gamil.com>
2023-06-14 11:35:52 -0400

    implementation of work with boards

---
+++ todo/goals/serializers/board.py
@@ -0,0 +1,92 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from goals.models import (
+    Board,
+    BoardParticipant
+)
+from rest_framework import serializers
+
+
+USER_MODEL = get_user_model()
+
+
+class BoardParticipantSerializer(serializers.ModelSerializer):
+    role = serializers.ChoiceField(
+        required=True,
+        choices=BoardParticipant.Role.choices
+    )
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=USER_MODEL.objects.all()
+    )
+
+    class Meta:
+        model = BoardParticipant
+        fields = '__all__'
+        read_only_fields = ('id', 'created', 'updated', 'board')
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    participants = BoardParticipantSerializer(many=True)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Board
+        fields = '__all__'
+        read_only_fields = ('id', 'created', 'updated')
+
+    def update(self, instance, validated_data):
+        user_owner = validated_data.pop('user')
+        participants_data = validated_data.pop('participants')
+        title = validated_data.pop('title')
+
+        new_participants = {participant['user']: participant for participant in participants_data}
+
+        with transaction.atomic():
+            board_participants = instance.participants.exclude(user=user_owner)
+            for old_participant in board_participants:
+                if old_participant.user not in new_participants:
+                    old_participant.delete()
+                else:
+                    if (new_participants[old_participant.user]['role'] != old_participant.role
+                            and new_participants[old_participant.user]['role'] != 1):
+                        old_participant.role = new_participants[old_participant.user]['role']
+                        old_participant.save()
+                    new_participants.pop(old_participant.user)
+
+            for participant in new_participants.values():
+                role = participant.get('role')
+                user = participant.get('user')
+
+                BoardParticipant.objects.create(
+                    user=user, board=instance, role=role
+                )
+            if instance.title != title:
+                instance.title = title
+            instance.save()
+
+        return instance
+
+
+class BoardCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Board
+        fields = '__all__'
+        read_only_fields = ('id', 'created', 'updated')
+
+    def create(self, validated_data):
+        owner = validated_data.pop('user')
+        board = Board.objects.create(**validated_data)
+        BoardParticipant.objects.create(
+            user=owner, board=board, role=BoardParticipant.Role.owner
+        )
+
+        return board
+
+
+class BoardListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = '__all__'
---
+++ todo/goals/views/board.py
@@ -0,0 +1,60 @@
+from django.db import transaction
+from django_filters.rest_framework import DjangoFilterBackend
+from goals.models import (
+    Board,
+    Goal
+)
+from goals.permissions import BoardPermission
+from goals.serializers.board import (
+    BoardCreateSerializer,
+    BoardListSerializer,
+    BoardSerializer
+)
+from rest_framework import (
+    filters,
+    generics,
+    pagination,
+    permissions
+)
+
+
+class BoardListView(generics.ListAPIView):
+    serializer_class = BoardListSerializer
+    permission_classes = [permissions.IsAuthenticated, BoardPermission]
+    pagination_class = pagination.LimitOffsetPagination
+
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
+
+    ordering = ['title']
+
+    search_fields = ['title']
+
+    def get_queryset(self):
+        return Board.objects.filter(
+            participants__user=self.request.user, is_deleted=False
+        )
+
+
+class BoardView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = BoardSerializer
+    permission_classes = [permissions.IsAuthenticated, BoardPermission]
+
+    def get_queryset(self):
+        return Board.objects.filter(
+            participants__user=self.request.user, is_deleted=False
+        )
+
+    def perform_destroy(self, instance):
+        with transaction.atomic():
+            instance.is_deleted = True
+            instance.save()
+            instance.categories.update(is_delete=True)
+            Goal.objects.filter(category__board=instance).update(
+                status=Goal.Status.archived
+            )
+        return instance
+
+
+class BoardCreateView(generics.CreateAPIView):
+    serializer_class = BoardCreateSerializer
+    permission_classes = [permissions.IsAuthenticated, BoardPermission]
