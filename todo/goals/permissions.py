895cd53f5a28555b43604b1fb2c16838b07d5502
mgd <mr.zizikk@gamil.com>
2023-06-15 12:07:54 -0400

    refactoring goal views, serializers, add custom permission class

--- todo/goals/permissions.py
+++ todo/goals/permissions.py
@@ -2,13 +2,19 @@ from goals.models import BoardParticipant
 from rest_framework import permissions
 
 
+permission_roles = (
+    BoardParticipant.Role.owner,
+    BoardParticipant.Role.writer
+)
+
+
 class BoardPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         attrs = {'user': request.user, 'board': obj}
 
         if request.method not in permissions.SAFE_METHODS:
-            attrs['role'] = obj.participants.model.Role.owner
+            attrs['role'] = BoardParticipant.Role.owner
         return BoardParticipant.objects.filter(**attrs).exists()
 
 
@@ -17,6 +23,25 @@ class GoalCategoryPermission(permissions.BasePermission):
         attrs = {'user': request.user, 'board': obj.board}
 
         if request.method not in permissions.SAFE_METHODS:
-            attrs['role__in'] = (obj.board.participants.model.Role.owner,
-                                 obj.board.participants.model.Role.writer,)
+            attrs['role__in'] = permission_roles
+        return BoardParticipant.objects.filter(**attrs).exists()
+
+
+class GoalPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        attrs = {'user': request.user, 'board': obj.category.board}
+
+        if request.method not in permissions.SAFE_METHODS:
+            attrs['role__in'] = permission_roles
+
+        return BoardParticipant.objects.filter(**attrs).exists()
+
+
+class CommentPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        attrs = {'user': request.user, 'board': obj.goal.category.board}
+
+        if request.method not in permissions.SAFE_METHODS:
+            attrs['role__in'] = permission_roles
+
         return BoardParticipant.objects.filter(**attrs).exists()
