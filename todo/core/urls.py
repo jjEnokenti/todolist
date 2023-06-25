daf0d1974cda096ab87d0037fb3f73f31fa9b504
mgd <mr.zizikk@gamil.com>
2023-06-25 00:24:58 -0400

    add name param to urls

--- todo/core/urls.py
+++ todo/core/urls.py
@@ -4,13 +4,13 @@ from .views import (
     UserChangePasswordView,
     UserCreateView,
     UserLoginView,
-    UserProfileView
+    UserProfileView,
 )
 
 
 urlpatterns = [
-    path('signup', UserCreateView.as_view()),
-    path('login', UserLoginView.as_view()),
-    path('profile', UserProfileView.as_view()),
-    path('update_password', UserChangePasswordView.as_view()),
+    path('signup', UserCreateView.as_view(), name='user_register'),
+    path('login', UserLoginView.as_view(), name='user_login'),
+    path('profile', UserProfileView.as_view(), name='user_profile'),
+    path('update_password', UserChangePasswordView.as_view(), name='update_password'),
 ]
