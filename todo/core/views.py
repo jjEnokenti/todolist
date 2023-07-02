564f5446115978b95e4a268917f61af58344e51c
mgd <mr.zizikk@gamil.com>
2023-07-02 17:07:35 -0400

    add docstring to post method

--- todo/core/views.py
+++ todo/core/views.py
@@ -38,6 +38,7 @@ class UserLoginView(generics.GenericAPIView):
         summary='authenticate user'
     )
     def post(self, request, *args, **kwargs):
+        """POST method handler."""
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
