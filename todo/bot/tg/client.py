d6d7bd4dd244642c6d551a3bdbf3c65061692df0
mgd <mr.zizikk@gamil.com>
2023-07-02 17:09:23 -0400

    add docstings, refactoring returns

--- todo/bot/tg/client.py
+++ todo/bot/tg/client.py
@@ -1,4 +1,5 @@
 import requests
+from requests import Response
 
 from bot.tg.dc import (
     GetUpdatesResponse,
@@ -14,18 +15,23 @@ class TgClient:
         self.token = token
 
     def get_url(self, method: str) -> str:
+        """Formatted url"""
         return f'https://api.telegram.org/bot{self.token}/{method}'
 
-    def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse | None:
+    def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse | Response:
+        """Get updates from telegram server."""
         url = self.get_url(method='getUpdates')
         params = {'offset': offset, 'timeout': timeout}
         data = requests.get(url=url, params=params)
         if data.ok:
             return get_updates_schema().load(data.json())
+        return data
 
-    def send_message(self, chat_id: int, text: str) -> SendMessageResponse | None:
+    def send_message(self, chat_id: int, text: str) -> SendMessageResponse | Response:
+        """Send message to telegram client."""
         url = self.get_url(method='sendMessage')
         params = {'chat_id': chat_id, 'text': text}
         data = requests.get(url=url, params=params)
         if data.ok:
             return send_message_schema().load(data.json())
+        return data
